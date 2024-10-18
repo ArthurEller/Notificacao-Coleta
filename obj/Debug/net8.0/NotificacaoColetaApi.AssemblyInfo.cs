016a4346a4a57f27577c853be988e07dce8168f6
Arthur Eller <arthurmachadoeller@hotmail.com>
2024-10-18 19:00:07 -0300

    fix

--- obj/Debug/net8.0/NotificacaoColetaApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/NotificacaoColetaApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NotificacaoColetaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+389ff035bd57408d34797634fa3074602d21dfec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c3b8c73f687577587faa442e52c25970e615396")]
 [assembly: System.Reflection.AssemblyProductAttribute("NotificacaoColetaApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NotificacaoColetaApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
